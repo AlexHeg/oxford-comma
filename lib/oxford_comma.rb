459d5b9eb045f4adafde424327087790bb75a5aa
Alexia <alexheg44@gmail.com>
2021-03-04 13:03:14 -0500

    commit tree c8850abf5ffb8c26ae6bd6497175aedd56bed92b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+    if array.size ==1 
+        return array.first 
+    
+    elsif array.size == 2
+        return array.join(" and ")
+
+    elsif array.size > 2
+        array[-1].insert(0, "and ")
+        array.join(", ")
+    
+
+    end    
+
 
 end
